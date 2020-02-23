f1fe07434e54e9c6daeac7ca446159ebaf19f155
booldook <booldook@gmail.com>
2020-02-23 12:32:56 +0900

    join.pug

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(session({
-  secret: 'my Salt!@#$',
+  secret: 'mySalt!@#$',
   resave: false,
   saveUninitialized: true,
   store: new sessionStore()
